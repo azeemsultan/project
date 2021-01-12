4ecba9a3becccbee4dbd3e1bc51d2d8d9febf71e
azeemsultan <43080738+azeemsultan@users.noreply.github.com>
2021-01-12 05:00:42 +0500

    cors

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ const connectDB       = require("./config/database");
 
 const app = express();
 app.use(cors());
+
+app.use(function(req,res,next)
+{
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+
 app.use("/api/admin", admin);
 app.use("/api/serviceprovider", serviceprovider);
 app.use("/api/customer", customer);
